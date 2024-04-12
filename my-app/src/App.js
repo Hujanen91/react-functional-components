c413022f35e3e508916f7343323423633b6d8bbd
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-04-12 09:46:14 +0000

    Do challenge in lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import css from './App.module.css';
+import NavBarChild from './components/NavBarChild';
+import NavBarForm from './components/NavBarForm';
 import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 
@@ -6,8 +8,10 @@ function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarChild />
       <NavBarSimple />
       <Sidebar />
+      <NavBarForm />
     </div>
   );
 }
