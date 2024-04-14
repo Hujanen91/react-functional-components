0302acf0bc0f3269ef4bc4bd6e339bda2b38add6
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-04-14 11:58:21 +0000

    Follow lessons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,15 @@
 import css from './App.module.css';
-
+import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
-import NavBarSimple from './components/NavBarSimple';
 import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarForm />
       <NavBarSimple />
+      <NavBarForm />
       <Sidebar />
       <Content />
     </div>
