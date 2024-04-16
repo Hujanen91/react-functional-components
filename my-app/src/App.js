5955fb5c8f2e91d90a710939859c404519bd7ef6
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-04-16 12:07:44 +0000

    Follow challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,13 @@ import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from "./components/Content";
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <ContentHooks />
       <NavBarSimple />
       <NavBarForm />
       <Sidebar />
