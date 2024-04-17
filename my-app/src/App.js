0949e764f9b5897d7d782312526dc2626166965a
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-04-17 11:53:28 +0000

    Add challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,22 @@
-import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
-import Sidebar from './components/Sidebar';
-import NavBarForm from './components/NavBarForm';
-import Content from "./components/Content";
-import ContentHooks from './components/ContentHooks';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <ContentHooks />
-      <NavBarSimple />
       <NavBarForm />
       <Sidebar />
-      <Content />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
